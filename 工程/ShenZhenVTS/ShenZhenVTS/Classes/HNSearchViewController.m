fe80fad9a5049b655b22fcb74163e836f5aa14ed
liuxh532 <786995209@qq.com>
2014-11-18 01:07:47 +0800

    增加搜索功能

--- 工程/ShenZhenVTS/ShenZhenVTS/Classes/HNSearchViewController.m
+++ 工程/ShenZhenVTS/ShenZhenVTS/Classes/HNSearchViewController.m
@@ -7,9 +7,17 @@
 //
 
 #import "HNSearchViewController.h"
+#import "UIView+AHKit.h"
+#import "JSONKit.h"
+#import "MBProgressHUD.h"
+#import "HNShipDynamicsModel.h"
+#import "HNShipDetailViewController.h"
 
-@interface HNSearchViewController ()
-
+@interface HNSearchViewController ()<UISearchBarDelegate,UITableViewDataSource,UITableViewDelegate>
+@property(nonatomic,strong) UISearchBar* searchBar;
+@property(nonatomic,strong) UITableView* tableView;
+@property (nonatomic, strong)NSMutableArray *shipList;
+@property (nonatomic, strong)NSArray *shipdynamicArray;
 @end
 
 @implementation HNSearchViewController
@@ -19,6 +27,133 @@
     // Do any additional setup after loading the view.
     self.view.backgroundColor = [UIColor whiteColor];
     self.navigationItem.title = @"艘船";
+    
+    self.searchBar = [[UISearchBar alloc]initWithFrame:CGRectMake(0.0, 0.0, self.view.bounds.size.width, 45)];
+    self.searchBar.delegate = self;
+    self.searchBar.placeholder=@"请输入搜索内容";
+    self.searchBar.showsCancelButton = YES;
+    [self.view addSubview:self.searchBar];
+    
+    self.tableView = [[UITableView alloc]init];
+    self.tableView.dataSource = self;
+    self.tableView.delegate = self;
+    [self.view addSubview:self.tableView];
+    
+    self.shipList = [[NSMutableArray alloc]init];
+    
+    self.shipdynamicArray = [NSArray arrayWithObjects:@"test",@"预抵",@"预离",@"正在抵港",@"正在离港",@"移泊",@"已靠泊",@"已锚泊",@"已离港",@"预抵",@"预抵", nil];
+}
+
+- (void)viewWillAppear:(BOOL)animated{
+    [super viewWillAppear:animated];
+    self.searchBar.frame = CGRectMake(0, 0, self.view.width, 45);
+    self.tableView.frame = CGRectMake(0, 45, self.view.width, self.view.height-45);
+    
+}
+
+//search Button clicked....
+- (void)searchBarSearchButtonClicked:(UISearchBar *)searchBar                     // called when keyboard search button pressed
+{
+    [searchBar resignFirstResponder];
+    [self loadMyData];
+}
+//cancel button clicked...
+- (void)searchBarCancelButtonClicked:(UISearchBar *) searchBar                    // called when cancel button pressed
+{
+    [searchBar resignFirstResponder];
+    
+}
+
+#pragma mark - tableView Delegate & DataSource
+- (CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath{
+    return 75.0;
+}
+-(NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section{
+    return [self.shipList count];
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath{
+    static NSString *reuseIdentifier = @"ArchivesCell";
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:reuseIdentifier];
+    if (!cell){
+        
+        cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleValue1 reuseIdentifier:reuseIdentifier];
+    }
+    HNShipDynamicsModel *model =self.shipList[indexPath.row];
+    cell.textLabel.text = model.shipname_cn;
+    cell.textLabel.textColor = [UIColor darkTextColor];
+    cell.detailTextLabel.text = [self.shipdynamicArray objectAtIndex:model.shipdynamic.integerValue ];
+    return cell;
+}
+
+-(void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    [tableView deselectRowAtIndexPath:indexPath animated:YES];
+    
+    NSInteger row = indexPath.row;
+    HNShipDetailViewController * vc = [[HNShipDetailViewController alloc]init];
+    vc.shipModel = [self.shipList objectAtIndex:row];
+    [self.navigationController pushViewController:vc animated:YES];
+    //    HNArchivesDecorateModel* model = self.modelList[row];
+    //    {
+    //        HNArchivesListViewController* dac = [[HNArchivesListViewController alloc]init];
+    //        dac.dID = model.declareId;
+    //        [self.navigationController pushViewController:dac animated:YES];
+    //    }
+    
+}
+
+#pragma mark -loadData
+-(void)loadMyData
+{
+    MBProgressHUD *hud=[MBProgressHUD showHUDAddedTo:self.view animated:YES];
+    hud.labelText = NSLocalizedString(@"Loading", nil);
+    NSMutableURLRequest *request = [[NSMutableURLRequest alloc] init];
+    
+    
+    request.URL = [NSURL URLWithString:[NSString stringWithFormat:@"http://202.104.126.36:8787/sz-web/plan/ShipController/checkShip?s=%d&e=%d&f=%@",1,18,self.searchBar.text]];
+    NSString *contentType = @"text/html";
+    [request addValue:contentType forHTTPHeaderField:@"Content-Type"];
+    [NSURLConnection sendAsynchronousRequest:request queue:[NSOperationQueue mainQueue] completionHandler:^(NSURLResponse *response, NSData *data, NSError *connectionError){
+        [self performSelectorOnMainThread:@selector(didloadMyData:) withObject:data waitUntilDone:YES];
+    }];
+}
+
+- (void)didloadMyData:(NSData *)data
+{
+    [MBProgressHUD hideHUDForView:self.view animated:YES];
+    
+    if (data)
+    {
+        [self.shipList removeAllObjects];
+        NSString *retStr = [[NSString alloc] initWithData:data encoding:NSUTF8StringEncoding];
+        NSLog(@"%@",retStr);
+        NSDictionary* dic = [retStr objectFromJSONString];
+        if (![dic count]) {
+            return;
+        }
+        NSNumber* total = [dic objectForKey:@"total"];
+        
+        if (total.intValue){//之后需要替换成status
+            NSArray* array = [dic objectForKey:@"shipDynamics"];
+            for (int i = 0; i<[array count]; i++) {
+                NSDictionary *dicData = [array objectAtIndex:i];
+                HNShipDynamicsModel *tModel = [[HNShipDynamicsModel alloc] init];
+                [tModel updateData:dicData];
+                [self.shipList addObject:tModel];
+            }
+            [self.tableView reloadData];
+        }
+        else
+        {
+            UIAlertView *alert = [[UIAlertView alloc] initWithTitle:NSLocalizedString(@"Login Fail", nil) message:NSLocalizedString(@"Please input correct username and password", nil) delegate:nil cancelButtonTitle:NSLocalizedString(@"OK", nil) otherButtonTitles: nil];
+            [alert show];
+        }
+    }
+    else{
+        UIAlertView *alert = [[UIAlertView alloc] initWithTitle:NSLocalizedString(@"Connection Error", nil) message:NSLocalizedString(@"Please check your network.", nil) delegate:nil cancelButtonTitle:NSLocalizedString(@"OK", nil) otherButtonTitles: nil];
+        [alert show];
+    }
 }
 
 - (void)didReceiveMemoryWarning {
